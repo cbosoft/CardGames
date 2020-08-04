f1584e25c6fdbb88460ee41b5b7c6bc891565ca4
Christopher Boyle <chrismjboyle@gmail.com>
2020-08-04 17:51:58 +0100

    Initial Commit

---
+++ CardGames/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  CardGames
+//
+//  Created by Christopher Boyle on 04/08/2020.
+//  Copyright © 2020 Christopher Boyle. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
