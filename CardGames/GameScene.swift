4342c2d826a80343c5934b84a1e799b1ac07bb39
Christopher Boyle <chrismjboyle@gmail.com>
2020-08-06 09:41:33 +0100

    Update gamescene to run game using table object

--- CardGames/GameScene.swift
+++ CardGames/GameScene.swift
@@ -11,61 +11,35 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var spinnyNode : SKShapeNode?
-    private var cards : [Card] = []
-    private var selectedCard : Card?
-    private var positions: [CardPosition] = []
-    
+    var table: Table? = nil
     
     override func didMove(to view: SKView) {
         // called when view is opened?
         
-        // layout cards here
-        // TODO
+        let hSize = CGSize(width: self.size.width*0.5, height: self.size.height*0.5)
+        self.table = SolitaireTable(size: hSize)
+        self.addChild(table!)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        // Pick up a card
-        var selected_z: CGFloat = -1.0
-        for card in self.cards {
-            if let z = card.point_hits(pt: pos) {
-                if z > selected_z {
-                    self.selectedCard = card
-                    selected_z = z
-                }
-            }
-        }
-        
-        if let card = self.selectedCard {
-            card.z = 100.0
-            card.run(action: SKAction.init(named: "ScaleUp")!)
+        if let table = self.table {
+            table.try_pick_up_card(here: pos)
         }
     }
     
     
     func touchMoved(toPoint pos : CGPoint) {
-        // Update card's position, if picked up
-        if let card = self.selectedCard {
-            card.set_position(fromPoint: pos)
+        if let table = self.table {
+            table.try_move_card(here: pos)
         }
     }
     
     
     func touchUp(atPoint pos : CGPoint) {
-        // TODO set down card on stack
-        if let card = self.selectedCard {
-            card.z = 0.0
-            card.run(action: SKAction.init(named: "ScaleDown")!)
-            for position in self.positions {
-                if let _ = position.point_hits(pt: pos) {
-                    card.set_position(fromPoint: position.get_position())
-                    break
-                }
-                // positions will never overlap, don't need to take zposition into account
-            }
+        if let table = self.table {
+            table.try_drop_card(here: pos)
         }
-        self.selectedCard = nil
     }
     
     
