380295ec92c8054116e151de5d6c37ecd58bb203
Christopher Boyle <chrismjboyle@gmail.com>
2020-08-07 13:21:32 +0100

    Add Suit and Value check for top stack

--- CardGames/SuitedTopStack.swift
+++ CardGames/SuitedTopStack.swift
@@ -11,9 +11,11 @@ import Foundation
 class SuitedTopStack: TopStack {
     
     private let suit: String
+    private let max_cards: Int
     
-    init(x: CGFloat, y: CGFloat, suit: String) {
+    init(x: CGFloat, y: CGFloat, suit: String, max_cards: Int = 13) {
         self.suit = suit
+        self.max_cards = max_cards
         super.init(x: x, y: y)
         let suitlabel = SKLabelNode(text: suit)
         let w = Card.size.width
@@ -30,5 +32,27 @@ class SuitedTopStack: TopStack {
         fatalError("init(coder:) has not been implemented")
     }
     
-    // TODO check suit of cards added using add_card method
+    func full() -> Bool {
+        return self.cards.count >= self.max_cards
+    }
+    
+    override func add_card(_ card: CardPosition) -> Bool {
+        if self.full() {
+            return false
+        }
+        
+        if let card = card as? Card {
+            if card.suit == self.suit {
+                
+                let next_value = Deck.values[self.cards.count]
+                if card.value != next_value {
+                    return false
+                }
+                
+                return super.add_card(card)
+            }
+        }
+        
+        return false
+    }
 }
