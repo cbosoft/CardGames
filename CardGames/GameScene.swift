a70bb725defd1f4962423aade5f21957ebfffa38
Christopher Boyle <chrismjboyle@gmail.com>
2020-08-15 17:11:03 +0100

    Move help and autocomplete to NSMenu
    More canonical way of providing help to user

--- CardGames/GameScene.swift
+++ CardGames/GameScene.swift
@@ -31,8 +31,6 @@ class GameScene: SKScene, Themeable {
     
     var table: Table? = nil
     var TableType: Table.Type = SolitaireTable.self
-    var help: SKNode? = nil
-    let help_text: [String] = ["r: Re-deal", "a: auto-complete", "m: Menu"]
     
     // MARK: didMove
     override func didMove(to view: SKView) {
@@ -42,10 +40,6 @@ class GameScene: SKScene, Themeable {
         
         self.table = self.TableType.init(size: size)
         self.addChild(table!)
-        
-        let right_click_rec = NSClickGestureRecognizer(target: self, action: #selector(self.rightClicked))
-        right_click_rec.buttonMask = 2
-        self.view!.addGestureRecognizer(right_click_rec)
         
         // add game menu to menu bar
         if let app = NSApp.delegate {
@@ -82,45 +76,10 @@ class GameScene: SKScene, Themeable {
         }
     }
     
-    
-    // MARK: Init Help Menu
-    func init_help() {
-        if self.help != nil {
-            return
-        }
-        
-        let help = SKShapeNode()
-        let dy: CGFloat = 35
-        print(dy)
-        help.path = CGPath(roundedRect: CGRect(x: -0.5*dy, y: 0, width: 200, height: CGFloat(self.help_text.count+1)*dy), cornerWidth: 5, cornerHeight: 5, transform: nil)
-        help.position = CGPoint(x: self.size.width*0.1, y: self.size.height*0.25)
-        help.strokeColor = help.fillColor
-        
-        var y: CGFloat = dy*0.5
-        for text in self.help_text.reversed() {
-            let label = SKLabelNode(text: text)
-            label.position = CGPoint(x: 0.0, y: y)
-            label.zPosition = 210
-            label.horizontalAlignmentMode = .left
-            y += dy
-            help.addChild(label)
-        }
-        
-        help.run(SKAction.fadeOut(withDuration: 0.0))
-        self.addChild(help)
-        self.help = help
-    }
-    
     // MARK: Mouse and touch
-    
     func touchDown(atPoint pos : CGPoint) {
         if let table = self.table {
-            if table.big_label.alpha == 1.0 {
-                self.show_help()
-            }
-            else {
-                table.try_pick_up_card(here: pos)
-            }
+            table.try_pick_up_card(here: pos)
         }
     }
     
@@ -153,20 +112,7 @@ class GameScene: SKScene, Themeable {
         self.touchUp(atPoint: event.location(in: self))
     }
     
-    @objc func rightClicked() {
-        self.auto_complete()
-    }
-    
     // MARK: Menu commands
-    
-    func redeal() {
-        self.table?.run(SKAction.fadeOut(withDuration: 0.2))
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            self.table?.redeal()
-            self.table?.run(SKAction.fadeIn(withDuration: 0.2))
-        }
-    }
-    
     @objc func back_to_menu(_ sender: Any) {
         self.back_to_menu()
     }
@@ -181,44 +127,6 @@ class GameScene: SKScene, Themeable {
                 scene.run(SKAction.fadeIn(withDuration: 0.3))
             }
             
-        }
-    }
-    
-    func show_help() {
-        if self.help == nil {
-            self.init_help()
-        }
-        
-        let help = self.help!
-        help.run(SKAction.fadeIn(withDuration: 0.1))
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            help.run(SKAction.fadeOut(withDuration: 0.5))
-        }
-    }
-    
-    func auto_complete() {
-        self.table?.auto_complete()
-    }
-    
-    // MARK: Key Input
-    override func keyDown(with event: NSEvent) {
-        // https://gist.github.com/swillits/df648e87016772c7f7e5dbed2b345066
-        switch event.keyCode {
-        //case 0x31:
-        //    if let label = self.label {
-        //        label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        //    }
-        case 0x2E:
-            self.back_to_menu()
-            
-        case 0x0F:
-            self.redeal()
-            
-        case 0x00:
-            self.auto_complete()
-            
-        default:
-            self.show_help()
         }
     }
     
