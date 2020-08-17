c0dc7c7abc0db104a93fc8a026f7c1bd39ba7033
Christopher Boyle <chrismjboyle@gmail.com>
2020-08-17 18:30:15 +0100

    Application terminate on window close

--- CardGames/AppDelegate.swift
+++ CardGames/AppDelegate.swift
@@ -40,5 +40,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
     
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
     
 }
